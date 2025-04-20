51ad9dc443d7a31d5c722d2b503952416ae69212
Mike Hurley <m.w.hurley@gmail.com>
2025-04-20 07:40:14 -0500

    array callout maker now also exposes the expand flag

--- javascript/lib/array-utils.js
+++ javascript/lib/array-utils.js
@@ -6,12 +6,13 @@ class ArrayUtils {
    * @param {string} calloutType - The type of callout, expected to be a value Obsidian allows in a callout's markdown.
    * @param {string} title - The callout's title.
    * @param {Array|DataArray} array - The data to put in a list in the callout.
+   * @param {boolean} [defaultToExpanded=true] - Flag that indicates if the callout starts opened or closed.
    */
-  insertDataviewCalloutFromArray(dv, calloutType, title, array) {
+  insertDataviewCalloutFromArray(dv, calloutType, title, array, defaultToExpanded = true) {
     const { ObsidianUtils } = customJS;
     
     const markdown = dv.markdownList(array);
-    const callout = ObsidianUtils.generateCalloutMarkdown(calloutType, title, markdown);
+    const callout = ObsidianUtils.generateCalloutMarkdown(calloutType, title, markdown, defaultToExpanded);
     dv.paragraph(callout);
   }
 }
